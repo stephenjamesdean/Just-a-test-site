3eeda335f9eb50b922193767013fa7e080dbc4e0
stephenjamesdean <stephen.james.dean@outlook.com.au>
2023-04-30 16:55:29 +1000

    color and size flags

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
+//Set variables
+let changeColor = true;   // set true to change the color
+let changeSize = true;   // set true to change the size
+
 
 const element = document.getElementById("parent");
 const words = document.getElementById("words");
 const content = words.innerHTML;
 
 let startIndex = 0;  //first char to edit - should be zero
-let endIndex = 40;  //last char to edit in batch 
+let endIndex = 52;  //last char to edit in batch 
 let indexChange = endIndex;  //size of starting batch - same as endIndex
 let indexIncrementChange = 4; //how many more characters are added to the change each loop
-let staticFontSize = 50;  //font size of first batch
-let fontChange = 1.3;  //font change each batch
+let staticFontSize = 55;  //font size of first batch
+let fontChange = 2.4;  //font change each batch
 let minimumFontSize = 9;   //minimum font size if loop continues a long time
 
-let changeColor = true;   //set this to change the color too
 let hexColor = "#2f3e5e";  //Starting color
 
-let loopLimit = content.length/(indexChange+endIndex);   //bit of math to choose how many loops to do - not accurate, but it stops it looping thousands of times.
-console.log(loopLimit);
-for (let i = 0; i < loopLimit; i++) {
-  //Setup
-  const extractedText = content.substring(startIndex, endIndex);
-  if (!extractedText){console.log(i);break;}  //break when last text is extracted
-  startIndex=endIndex;
-  endIndex=endIndex+indexChange;
-  indexChange=indexChange+indexIncrementChange;
+for (let i = 0; i < content.length; i++) {
+    
+    const extractedText = content.substring(startIndex, endIndex);
+    if (!extractedText){  //no more characters in the text box
+        break; //break after last text is extracted
+    } 
 
-  // Create a new div and set its innerHTML to the extracted text
-  const newDiv = document.createElement("span");
-  newDiv.innerHTML = extractedText;
-  
-  //SIZE
-  staticFontSize = staticFontSize-fontChange;
-  if (staticFontSize<minimumFontSize){staticFontSize = minimumFontSize;}
-  var size = staticFontSize.toString() + "px";
-  newDiv.style.fontSize = size;
-  newDiv.style.lineHeight = 1;
-  
-  
-  //COLOR
-  if(changeColor){
-    var red = parseInt(hexColor.slice(1, 3), 16);
-    var green = parseInt(hexColor.slice(3, 5), 16);
-    var blue = parseInt(hexColor.slice(5, 7), 16);
+    // Increment batch numbers
+    startIndex=endIndex;
+    endIndex=endIndex+indexChange;
+    indexChange=indexChange+indexIncrementChange;
 
-    // Lighten the color by adding or subtracting from each RGB value
-    var lightenedRed = Math.min(255, red + 4); if (lightenedRed > 240){lightenedRed=240}; if (lightenedRed < 20){lightenedRed=20};
-    var lightenedGreen = Math.min(255, green + 4); if (lightenedGreen > 240){lightenedGreen=240}; if (lightenedGreen < 20){lightenedGreen=20}; 
-    var lightenedBlue = Math.min(255, blue + 6); if (lightenedBlue > 250){lightenedBlue=250}; if (lightenedBlue < 20){lightenedBlue=20};
+    // Create a new div and set its innerHTML to the extracted text
+    const newSpan = document.createElement("span");
+    newSpan.innerHTML = extractedText;
 
-    // Convert the lightened RGB values back to hex
-    hexColor = `#${lightenedRed.toString(16)}${lightenedGreen.toString(16)}${lightenedBlue.toString(16)}`;
-    newDiv.style.color = hexColor;
-  }
-  
-  // Insert the new div into the DOM
-  element.appendChild(newDiv);
-  
+    //SIZE
+    if(changeSize){
+        staticFontSize = staticFontSize-fontChange;
+        if (staticFontSize<minimumFontSize){staticFontSize = minimumFontSize;}
+        var size = staticFontSize.toString() + "px";
+        newSpan.style.fontSize = size;
+        newSpan.style.lineHeight = 1;
+    }
+
+    //COLOR
+    if(changeColor){
+        var red = parseInt(hexColor.slice(1, 3), 16);
+        var green = parseInt(hexColor.slice(3, 5), 16);
+        var blue = parseInt(hexColor.slice(5, 7), 16);
+
+        // Lighten the color by adding or subtracting from each RGB value
+        var lightenedRed = Math.min(255, red + 4); if (lightenedRed > 240){lightenedRed=240}; if (lightenedRed < 20){lightenedRed=20};
+        var lightenedGreen = Math.min(255, green + 4); if (lightenedGreen > 240){lightenedGreen=240}; if (lightenedGreen < 20){lightenedGreen=20}; 
+        var lightenedBlue = Math.min(255, blue + 6); if (lightenedBlue > 250){lightenedBlue=250}; if (lightenedBlue < 20){lightenedBlue=20};
+        hexColor = `#${lightenedRed.toString(16)}${lightenedGreen.toString(16)}${lightenedBlue.toString(16)}`;
+    }
+
+    newSpan.style.color = hexColor;
+    // Insert the new div into the DOM
+    element.appendChild(newSpan);
+
 
 }
 words.remove();
